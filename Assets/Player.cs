b04c18430e7f54e3dc8b4d306c446b123d502f45
Lee Scroggins <lscrggns@yahoo.com>
2021-09-01 20:27:19 -0600

    Updated script.

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Reset MoveDelta
+        moveDelta = Vector3.zero;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Debug.Log(x);
+        Debug.Log(y);
+    }
+}
